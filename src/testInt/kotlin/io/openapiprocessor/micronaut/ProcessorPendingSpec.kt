d21da04b3d81bfdaad8767b89f9056f7be3dc15c
Martin Hauner <martin.hauner@gmx.net>
2022-05-23 08:39:40 +0200

    migrate testInt to kotlin

---
+++ src/testInt/kotlin/io/openapiprocessor/micronaut/ProcessorPendingSpec.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2022 https://github.com/openapi-processor/openapi-processor-micronaut
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiprocessor.micronaut
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.engine.spec.tempdir
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.openapiprocessor.core.parser.ParserType.INTERNAL
+import io.openapiprocessor.test.FileSupport
+import io.openapiprocessor.test.TestSet
+import io.openapiprocessor.test.TestSetRunner
+
+/**
+ * helper to run selected integration tests.
+ */
+class ProcessorPendingSpec: StringSpec({
+
+    for (testSet in sources()) {
+        "native - $testSet".config(enabled = false) {
+            val folder = tempdir()
+
+            val support = FileSupport(
+                ProcessorPendingSpec::class.java,
+                testSet.inputs, testSet.generated)
+
+            TestSetRunner(testSet, support)
+            .runOnNativeFileSystem(folder)
+            .shouldBeTrue()
+        }
+    }
+})
+
+private fun sources(): Collection<TestSet> {
+    setTestHeader()
+
+    return listOf(
+        testSet("bean-validation-introspected", INTERNAL, API_30),
+        testSet("bean-validation-introspected", INTERNAL, API_31)
+    )
+}
