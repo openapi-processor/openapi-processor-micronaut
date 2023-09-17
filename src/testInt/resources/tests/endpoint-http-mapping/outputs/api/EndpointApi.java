c5ff3556c80ff39aab45071dff916c6176f6d326
Martin Hauner <martin.hauner@gmx.net>
2023-09-17 11:24:22 +0200

    check all http methods

--- src/testInt/resources/tests/endpoint-http-mapping/outputs/api/EndpointApi.java
+++ src/testInt/resources/tests/endpoint-http-mapping/outputs/api/EndpointApi.java
@@ -1,24 +1,40 @@
 package generated.api;
 
 import generated.support.Generated;
+import io.micronaut.http.annotation.Delete;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Head;
+import io.micronaut.http.annotation.Options;
 import io.micronaut.http.annotation.Patch;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Put;
+import io.micronaut.http.annotation.Trace;
 
 @Generated(value = "openapi-processor-micronaut", version = "test")
 public interface EndpointApi {
 
+    @Delete(uri = "/endpoint")
+    void deleteEndpoint();
+
     @Get(uri = "/endpoint")
     void getEndpoint();
 
-    @Put(uri = "/endpoint")
-    void putEndpoint();
+    @Head(uri = "/endpoint")
+    void headEndpoint();
+
+    @Options(uri = "/endpoint")
+    void optionsEndpoint();
+
+    @Patch(uri = "/endpoint")
+    void patchEndpoint();
 
     @Post(uri = "/endpoint")
     void postEndpoint();
 
-    @Patch(uri = "/endpoint")
-    void patchEndpoint();
+    @Put(uri = "/endpoint")
+    void putEndpoint();
+
+    @Trace(uri = "/endpoint")
+    void traceEndpoint();
 
 }
